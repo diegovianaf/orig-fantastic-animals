57f3300449f00b8207231a4e79d6b99149651d72
Diego Viana <diegovianaf7@gmail.com>
2022-02-07 17:10:32 -0300

    feat: add modal for sign in

---
+++ js/modules/modal.js
@@ -0,0 +1,23 @@
+export default function initModal() {
+  const openButton = document.querySelector('[data-modal="open"]')
+  const closeButton = document.querySelector('[data-modal="close"]')
+  const modalContainer = document.querySelector('[data-modal="container"]')
+
+  if (openButton && closeButton && modalContainer) {
+
+    function toggleModal(event) {
+      event.preventDefault()
+      modalContainer.classList.toggle('active')
+    }
+
+    function clickOutModal(event) {
+      if (event.target === this) {
+        toggleModal(event)
+      }
+    }
+
+    openButton.addEventListener('click', toggleModal)
+    closeButton.addEventListener('click', toggleModal)
+    modalContainer.addEventListener('click', clickOutModal)
+  }
+}
--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@ import initSmoothScroll from './modules/smooth-scroll.js'
 import initScrollAnimation from './modules/scroll-animation.js'
 import initAccordion from './modules/accordion.js'
 import initTabNav from './modules/tabnav.js'
+import initModal from './modules/modal.js'
 
 initSmoothScroll()
 initScrollAnimation()
 initAccordion()
 initTabNav()
+initModal()
