8b80399721bc44a258daf780030dcc28451b1ccd
Diego Viana <diegovianaf7@gmail.com>
2022-02-08 13:28:16 -0300

    feat: add tooltip on the map image

--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@ import initScrollAnimation from './modules/scroll-animation.js'
 import initAccordion from './modules/accordion.js'
 import initTabNav from './modules/tabnav.js'
 import initModal from './modules/modal.js'
+import initTooltip from './modules/tooltip.js'
 
 initSmoothScroll()
 initScrollAnimation()
 initAccordion()
 initTabNav()
 initModal()
+initTooltip()
