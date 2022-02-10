69c0232ba5e4c4b2a89f62b60f2ba0dcb44d9604
Diego Viana <diegovianaf7@gmail.com>
2022-02-10 17:17:46 -0300

    feat: add working hours

---
+++ js/modules/operating.js
@@ -0,0 +1,16 @@
+export default function InitOperating() {
+  const operating = document.querySelector('[data-week]')
+  const weekDays = operating.dataset.week.split(',').map(Number)
+  const dayTime = operating.dataset.time.split(',').map(Number)
+
+  const dateNow = new Date()
+  const dayNow = dateNow.getDay()
+  const timeNow = dateNow.getUTCHours() - 3
+
+  const weekOpened = weekDays.indexOf(dayNow) !== -1
+  const timeOpened = (timeNow >= dayTime[0] && timeNow < dayTime[1])
+
+  if(weekOpened && timeOpened) {
+    operating.classList.add('opened')
+  }
+}
--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ import initTooltip from './modules/tooltip.js'
 import initDropdownMenu from './modules/dropdown-menu.js'
 import initMenuMobile from './modules/menu-mobile.js'
 import initAnimaNumbers from './modules/anima-numbers.js'
+import InitOperating from './modules/operating.js'
 
 initSmoothScroll()
 initScrollAnimation()
@@ -17,3 +18,4 @@ initTooltip()
 initDropdownMenu()
 initMenuMobile()
 initAnimaNumbers()
+InitOperating()
