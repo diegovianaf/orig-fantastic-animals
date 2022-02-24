3bef575986eed120f421b8cf5e27a4c776aafa56
Diego Viana <diegovianaf7@gmail.com>
2022-02-24 15:53:10 -0300

    fix: put the correct data-tab

--- js/script.js
+++ js/script.js
@@ -16,7 +16,7 @@ smoothScroll.init()
 const accordion = new Accordion('[data-anime="accordion"] dt')
 accordion.init()
 
-const tabNav = new TabNav('[data-tab="menu"] li', '[data-tab="content"] section')
+const tabNav = new TabNav('[data-tab="navbar"] li', '[data-tab="content"] section')
 tabNav.init()
 
 const modal = new Modal('[data-modal="open"]', '[data-modal="close"]', '[data-modal="container"]')
